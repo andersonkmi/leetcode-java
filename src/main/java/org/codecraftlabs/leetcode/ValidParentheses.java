111006ccc7346433f424c3ea954aa1d6ccf96279
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2020-10-07 20:43:28 +0200

    New code

---
+++ src/main/java/org/codecraftlabs/leetcode/ValidParentheses.java
@@ -0,0 +1,10 @@
+package org.codecraftlabs.leetcode;
+
+public class ValidParentheses {
+    public boolean isValid(String input) {
+        if (input == null || input.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/test/java/org/codecraftlabs/leetcode/ValidParenthesesTests.java
@@ -0,0 +1,20 @@
+package org.codecraftlabs.leetcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ValidParenthesesTests {
+    private ValidParentheses cut;
+    
+    @BeforeEach
+    void setup() {
+        cut = new ValidParentheses();
+    }
+
+    @Test
+    void validateNullString() {
+        assertTrue(cut.isValid(null));
+    }
+}
