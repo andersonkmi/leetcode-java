b921d705a91943b66dcd805455d7d0b30e5db5d5
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2025-10-22 11:28:27 +0200

    Refactoring Pair class:

--- src/main/java/org/codecraftlabs/leetcode/Pair.java
+++ src/main/java/org/codecraftlabs/leetcode/Pair.java
@@ -1,23 +1,4 @@
 package org.codecraftlabs.leetcode;
 
-import javax.annotation.Nonnull;
-
-public class Pair<T, U> {
-    private final T first;
-    private final U second;
-
-    public Pair(T first, U second) {
-        this.first = first;
-        this.second = second;
-    }
-
-    @Nonnull
-    public final T getFirst() {
-        return first;
-    }
-
-    @Nonnull
-    public final U getSecond() {
-        return second;
-    }
+public record Pair<T, U>(T first, U second) {
 }
