dd331ca14373d0dfec3471acfb6fb5bad32d377a
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2025-10-22 23:32:56 +0200

    Add unit test

--- src/test/java/org/codecraftlabs/leetcode/UniqueCharFinderTest.java
+++ src/test/java/org/codecraftlabs/leetcode/UniqueCharFinderTest.java
@@ -4,6 +4,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class UniqueCharFinderTest {
     private UniqueCharFinder uniqueCharFinder;
 
@@ -23,5 +25,12 @@ public class UniqueCharFinderTest {
         var results = this.uniqueCharFinder.getUniqueCharacters("a");
         Assertions.assertThat(results).hasSize(1);
         Assertions.assertThat(results).contains('a');
+    }
+
+    @Test
+    public void testMultipleCharString() {
+        var results = this.uniqueCharFinder.getUniqueCharacters("abssswed");
+        Assertions.assertThat(results).hasSize(6);
+        Assertions.assertThat(results).containsAll(List.of('a', 'b', 's', 'w', 'e', 'd'));
     }
 }
