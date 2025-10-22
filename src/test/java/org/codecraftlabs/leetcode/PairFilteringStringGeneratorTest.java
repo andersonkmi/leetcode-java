e54f865b91d93a521f23b77aabf34c7cf2c32e6c
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2025-10-22 23:13:47 +0200

    Add one more test case

--- src/test/java/org/codecraftlabs/leetcode/PairFilteringStringGeneratorTest.java
+++ src/test/java/org/codecraftlabs/leetcode/PairFilteringStringGeneratorTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
+import java.util.Set;
 
 public class PairFilteringStringGeneratorTest {
     private PairFilteringStringGenerator pairFilteringStringGenerator;
@@ -18,5 +19,12 @@ public class PairFilteringStringGeneratorTest {
     public void testWithEmptyPairSet() {
         var results = this.pairFilteringStringGenerator.filterCharPairsFromString(Collections.emptySet(), "abdefc");
         Assertions.assertThat(results).isEmpty();
+    }
+
+    @Test
+    public void testWithValidPairSet() {
+        var results = this.pairFilteringStringGenerator.filterCharPairsFromString(Set.of(new Pair<>('a', 'b')), "abaabcda");
+        Assertions.assertThat(results).hasSize(1);
+        Assertions.assertThat(results).contains("abaaba");
     }
 }
