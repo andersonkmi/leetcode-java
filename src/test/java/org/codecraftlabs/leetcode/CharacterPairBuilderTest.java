7ae86b08c5b7a75cfb4032565eb2fcb216b46436
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2025-10-22 11:48:12 +0200

    Add one more test

--- src/test/java/org/codecraftlabs/leetcode/CharacterPairBuilderTest.java
+++ src/test/java/org/codecraftlabs/leetcode/CharacterPairBuilderTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Set;
 
 public class CharacterPairBuilderTest {
     private CharacterPairBuilder characterPairBuilder;
@@ -31,5 +32,30 @@ public class CharacterPairBuilderTest {
         var results = this.characterPairBuilder.generatePairs(List.of('a', 'd'));
         Assertions.assertThat(results).hasSize(1);
         Assertions.assertThat(results).containsOnly(new Pair<>('a', 'd'));
+    }
+
+    @Test
+    public void testPairGenerationListWithABD() {
+        var results = this.characterPairBuilder.generatePairs(List.of('a', 'b', 'd'));
+        Assertions.assertThat(results).hasSize(3);
+        Assertions.assertThat(results).containsAll(
+                Set.of(new Pair<>('a', 'b'),
+                        new Pair<>('a', 'd'),
+                        new Pair<>('b', 'd'))
+        );
+    }
+
+    @Test
+    public void testPairGenerationListWithABDG() {
+        var results = this.characterPairBuilder.generatePairs(List.of('a', 'b', 'd', 'g'));
+        Assertions.assertThat(results).hasSize(6);
+        Assertions.assertThat(results).containsAll(
+                Set.of(new Pair<>('a', 'b'),
+                        new Pair<>('a', 'd'),
+                        new Pair<>('a', 'g'),
+                        new Pair<>('b', 'd'),
+                        new Pair<>('b', 'g'),
+                        new Pair<>('d', 'g'))
+        );
     }
 }
