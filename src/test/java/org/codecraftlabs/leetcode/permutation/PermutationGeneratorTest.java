1b5e0dd7bc731b2bb5ef6cb1ca556d4dcedf4998
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2025-10-23 10:32:21 +0200

    Add one more unit test

--- src/test/java/org/codecraftlabs/leetcode/permutation/PermutationGeneratorTest.java
+++ src/test/java/org/codecraftlabs/leetcode/permutation/PermutationGeneratorTest.java
@@ -35,4 +35,19 @@ public class PermutationGeneratorTest {
         expected.add(values);
         Assertions.assertThat(results).isEqualTo(expected);
     }
+
+    @Test
+    public void testWithTwoNumberArray() {
+        int[] numbers = {1, 2};
+        var results = this.permutationGenerator.generate(numbers);
+        Assertions.assertThat(results).hasSize(2);
+
+        // Verifies the list contents
+        List<List<Integer>> expected = new ArrayList<>();
+        List<Integer> line1 = List.of(1, 2);
+        List<Integer> line2 = List.of(2, 1);
+        expected.add(line1);
+        expected.add(line2);
+        Assertions.assertThat(results).isEqualTo(expected);
+    }
 }
