61aad1a7e48b9cc51f03839638c3b1518a43d96a
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2026-01-06 21:49:33 +0100

    Add initial example for triplet sum

---
+++ src/test/java/org/codecraftlabs/leetcode/tripletsum/TripletSumTest.java
@@ -0,0 +1,27 @@
+package org.codecraftlabs.leetcode.tripletsum;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TripletSumTest {
+    private TripletSum tripletSum;
+
+    @BeforeEach
+    public void setup() {
+        this.tripletSum = new TripletSum();
+    }
+
+    @Test
+    public void testTripletSumBruteForce() {
+        List<List<Integer>> expectedResults = new ArrayList<>();
+        expectedResults.add(List.of(-3, 1, 2));
+        expectedResults.add(List.of(-1, 0, 1));
+
+        List<List<Integer>> results = this.tripletSum.tripletSumBruteForce(List.of(0, -1, 2, -3, 1));
+        Assertions.assertThat(results).isEqualTo(expectedResults);
+    }
+}
