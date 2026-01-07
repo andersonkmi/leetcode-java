fecf2dc66b854cf457e5ecf23380df70a3b5121c
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2026-01-07 08:18:34 +0100

    Add pair sum implementation

--- src/main/java/org/codecraftlabs/leetcode/pairsum/PairSum.java
+++ src/main/java/org/codecraftlabs/leetcode/pairsum/PairSum.java
@@ -6,6 +6,19 @@ import java.util.List;
 class PairSum {
     @Nonnull
     List<Integer> pairSum(@Nonnull List<Integer> numbers, int target) {
+        int leftIndex = 0;
+        int rightIndex = numbers.size() - 1;
+        while (leftIndex < rightIndex) {
+            int sum = numbers.get(leftIndex) + numbers.get(rightIndex);
+
+            if (sum < target) {
+                leftIndex++;
+            } else if (sum > target) {
+                rightIndex--;
+            } else {
+                return List.of(numbers.get(leftIndex), numbers.get(rightIndex));
+            }
+        }
         return List.of();
     }
 }
