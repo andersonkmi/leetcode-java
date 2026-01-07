ba34f6d30a73e9fdbef3c91b022f3c4ffc39ba8c
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2026-01-07 08:10:09 +0100

    Add initial empty PairSumSorted implementation

---
+++ src/main/java/org/codecraftlabs/leetcode/pairsum/PairSum.java
@@ -0,0 +1,11 @@
+package org.codecraftlabs.leetcode.pairsum;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+
+class PairSum {
+    @Nonnull
+    List<Integer> pairSum(@Nonnull List<Integer> numbers, int target) {
+        return List.of();
+    }
+}
---
+++ src/test/java/org/codecraftlabs/leetcode/pairsum/PairSumTest.java
@@ -0,0 +1,23 @@
+package org.codecraftlabs.leetcode.pairsum;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class PairSumTest {
+    private PairSum pairSum;
+
+    @BeforeEach
+    public void setup() {
+        this.pairSum = new PairSum();
+    }
+
+    @Test
+    public void test_empty_list_should_return_empty_result() {
+        var result = this.pairSum.pairSum(List.of(), 3);
+        Assertions.assertThat(result).isEmpty();
+    }
+
+}
